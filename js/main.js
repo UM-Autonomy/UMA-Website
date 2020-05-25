6f276cbc9cc15c8a639849c1546950e7da8fcb9b
Rimaz Khan <rimazk@umich.edu>
2020-05-25 16:32:42 -0400

    remove and clean up old code

--- js/main.js
+++ js/main.js
@@ -1,39 +1,14 @@
-(function () {
+$(document).ready(function () {
 	var header = document.getElementById("mainHeader");
 
+	// Change the header of the page
 	function changeHeader() {
 		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 		header.classList.toggle("header-background", scrollTop >= 50 || document.body.classList.contains("nav-open"));
 	}
 
-	var didScroll = false;
-
 	$(window).scroll(function () {
-		didScroll = true;
-	});
-
-	setInterval(function() {
-		if (didScroll) {
-			didScroll = false;
-			changeHeader();
-		}
-	}, 100);
-
-	changeHeader();
-
-	document.getElementById("open-nav").addEventListener("click", function (event) {
-		event.preventDefault();
-		document.body.classList.toggle("nav-open");
 		changeHeader();
 	});
 
-	$("a[href*=\\#]").on("click", function (event) {
-		if(this.pathname === window.location.pathname) {
-			event.preventDefault();
-
-			$("html, body").animate({
-				scrollTop: $(this.hash).offset().top
-			}, 500);
-		}
-	});
 })();
